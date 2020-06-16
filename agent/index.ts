aa0c09a46b905e493cd499dc8e8dd3e7cdc71c24
yueluo <578903564@qq.com>
2020-06-16 14:42:18 +0800

    Update index.ts

--- agent/index.ts
+++ agent/index.ts
@@ -1,12 +1,12 @@
 let Il2cppApi: any = {
     il2cpp_domain_get: ['pointer', []],
-    il2cpp_domain_get_assemblies: ['pointer', ['pointer', 'uint']],
+    il2cpp_domain_get_assemblies: ['pointer', ['pointer', 'pointer']],
     il2cpp_thread_current: ['pointer', []],
     il2cpp_thread_attach: ['pointer', ['pointer']],
     il2cpp_thread_detach: ['void', ['pointer']],
     il2cpp_assembly_get_image: ['pointer', ['pointer']],
     il2cpp_image_get_assembly: ['pointer', ['pointer']],
-    il2cpp_image_get_name: ['pointer', ['pointer']], //char, char*?
+    il2cpp_image_get_name: ['pointer', ['pointer']],
     il2cpp_image_get_filename: ['pointer', ['pointer']],
     il2cpp_image_get_class_count: ['uint', ['pointer']],
     il2cpp_image_get_class: ['pointer', ['pointer', 'uint']],
@@ -20,12 +20,22 @@ let Il2cppApi: any = {
 
 for (const key in Il2cppApi) {
     if (Il2cppApi.hasOwnProperty(key)) {
-        const exp_name = Il2cppApi[key];
-        const addr = Module.findExportByName(null, exp_name);
+        const addr = Module.findExportByName(null, key);
         Il2cppApi[key] = !addr
-            ? () => { throw new Error('Export not found: ' + exp_name) }
-            : Il2cppApi[key] = new NativeFunction(addr, Il2cppApi[key][0], Il2cppApi[key][1], "mscdecl");
+            ? () => { throw new Error('Export not found: ' + key) }
+            : Il2cppApi[key] = new NativeFunction(addr, Il2cppApi[key][0], Il2cppApi[key][1]);
     }
 }
 
 let Il2CppThread_Ptr = Il2cppApi.il2cpp_thread_attach(Il2cppApi.il2cpp_domain_get());
+
+if (Il2CppThread_Ptr) {
+    let num = Memory.alloc(8);
+    let assemblies = Il2cppApi.il2cpp_domain_get_assemblies(Il2cppApi.il2cpp_domain_get(), num);
+
+    for (let index = 0; index < num.readU32(); index++) {
+        let asm = Il2cppApi.il2cpp_assembly_get_image(assemblies.add(Process.pointerSize * index).readPointer());
+        let name = Il2cppApi.il2cpp_image_get_name(asm);
+        console.log(name.readAnsiString());
+    }
+}
